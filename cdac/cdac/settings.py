f6947130dad811c0a569dd83e1dad742b697cf11
katkamrachana <katkam.rachana@gmail.com>
2017-09-18 23:24:52 +0530

    template updated

--- cdac/cdac/settings.py
+++ cdac/cdac/settings.py
@@ -27,7 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 # Application definition
 
 INSTALLED_APPS = [
