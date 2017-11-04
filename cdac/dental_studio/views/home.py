038e05d9348e4df43bee8ce59c2d78dcc350e899
katkamrachana <katkam.rachana@gmail.com>
2017-11-04 18:32:11 +0530

    homepage updates

--- cdac/dental_studio/views/home.py
+++ cdac/dental_studio/views/home.py
@@ -9,7 +9,7 @@ from django.template import loader
 
 def welcome(request):
     # return HttpResponse("Welcome message..")
-    template = loader.get_template('dental_studio/homepage.html')
+    template = loader.get_template('dental_studio/onepage.html')
     context = {
         'title': "Welcome"
     }
