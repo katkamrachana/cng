5ffb54f87f9ad08f55b2df6084ced322f47afed2
katkamrachana <katkam.rachana@gmail.com>
2017-11-24 12:41:04 +0530

    template tags and testimonial add form

---
+++ cdac/dental_studio/templatetags/cdac_tags.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+from django.urls import reverse
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from dental_studio.models import *
+from django.template import Library
+
+register = Library()
+
+@register.assignment_tag
+def get_testimonials():
+    t = Testimonial.testimonials()
+    return t
+
+@register.inclusion_tag('dental_studio/add_testimonial.html', takes_context=True)
+def add_testimonials():
+    print "\nHERE"
+    return {"HERE": "HELLO"}
+
+@register.assignment_tag
+def get_form(form_name):
+    from dental_studio.forms import TestimonialForm
+    form = TestimonialForm()
+    all_emails = Testimonial.get_email_list()
+    print "\n AALL EMAILS: ", all_emails
+    return (form ,all_emails)
