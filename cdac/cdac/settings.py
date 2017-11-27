1f74874bace8db76843c3c227b556bfe28d1343d
katkamrachana <katkam.rachana@gmail.com>
2017-11-27 23:14:42 +0530

    updates

--- cdac/cdac/settings.py
+++ cdac/cdac/settings.py
@@ -15,13 +15,22 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&365tehh@kack^%k5)87kpyb)flga*57y23$f3ye-6_t6d^d76'
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- cdac/cdac/wsgi.py
+++ cdac/cdac/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cdac.settings")
+
+# application = get_wsgi_application()
+
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cdac.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
