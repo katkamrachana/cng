5ec1fa30be51b9f7426d438263c8af7ee5d934cc
katkamrachana <katkam.rachana@gmail.com>
2017-11-27 23:49:40 +0530

    db url changed to postgres

--- cdac/cdac/settings.py
+++ cdac/cdac/settings.py
@@ -33,7 +33,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -93,10 +93,9 @@ WSGI_APPLICATION = 'cdac.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
