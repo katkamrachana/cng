7f19bda5bd63046f0c32c163c6b9f8fe75196d43
Kedar Aitawdekar <kedar2a@gmail.com>
2017-11-28 13:08:28 +0530

    modifies structure of dirs

--- cdac/wsgi.py
+++ cdac/wsgi.py
@@ -17,7 +17,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cdac.cdac.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cdac.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
