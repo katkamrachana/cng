ef9022b464b65fa86c445ff6107553a094cafe99
Kedar Aitawdekar <kedar2a@gmail.com>
2017-11-28 11:40:42 +0530

    added migrations

--- cdac/cdac/settings.py
+++ cdac/cdac/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 
